12ee47bb5d332ac5bb557021d9e4da1811bca749
zettacamp-irwan-cahyo <140873255+zettacamp-irwan-cahyo@users.noreply.github.com>
2023-08-25 14:51:22 +0700

    basic javascript

---
+++ basicJS.js
@@ -0,0 +1,38 @@
+// 2 favourite book
+let book1 = 'Rich Dad Poor Dad';
+const book2 = 'Bill Gates';
+
+book1 = 'Harry Potter';
+
+// book2 = 'cash flow quadrant';
+
+let finalBook = `${book1} better than ${book2}`;
+console.log(finalBook);
+
+// array
+const arr = ['ardi', 1, true];
+
+// console.log(Array.isArray(arr));
+// console.log(typeof arr[0]);
+// console.log(typeof arr[1]);
+// console.log(typeof arr[2]);
+
+function add(a, b) {
+  console.log(a + b);
+}
+
+// add(2, 6);
+
+// object
+const mahasiswa = {
+  nama: 'Irwan Cahyo S',
+  kelas: 12,
+  aktif: true,
+  ipk: 3.55,
+};
+
+// console.log(typeof mahasiswa);
+// console.log(typeof mahasiswa.nama);
+// console.log(typeof mahasiswa.kelas);
+// console.log(typeof mahasiswa.aktif);
+// console.log(typeof mahasiswa.ipk);
